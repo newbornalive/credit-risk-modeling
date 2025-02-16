40723893236663902a9ad3b86ba2900233e2cd51
Erica Yang <ericayang.work@gmail.com>
2025-02-16 02:43:18 -0500

    Update download_data.py

--- data/download_data.py
+++ data/download_data.py
@@ -1,1 +1,8 @@
+import kaggle
+import os
+
+# Set up Kaggle API (you need to get your Kaggle API key)
+os.system("kaggle competitions download -c GiveMeSomeCredit -p data/")
+
+print("Dataset downloaded successfully!")
 
